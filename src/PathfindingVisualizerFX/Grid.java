3b2a3c4e2824bab6a6c22ee405f983fa0994f764
Mike Cao <mc2522@rit.edu>
2020-07-04 17:51:54 -0400

    preset available

--- src/PathfindingVisualizerFX/Grid.java
+++ src/PathfindingVisualizerFX/Grid.java
@@ -1,4 +1,117 @@
 package PathfindingVisualizerFX;
 
+import java.util.Arrays;
+
 public class Grid {
+    // dimension of the grid (length x length)
+    private final int DIM = 20;
+    // number code of empty node
+    private final int EMPTY_NODE = 0;
+    // number code of start node
+    private final int START_NODE = 1;
+    // number code of end node
+    private final int END_NODE = 2;
+    // number code of obstacle node
+    private final int OBSTACLE_NODE = 3;
+    // 2D array to represent grid
+    private int [][] grid;
+    // coordinates for start node
+    private int [] startCoordinates;
+    // coordinates for end node
+    private int [] endCoordinates;
+
+    /**
+     * Constructor for Grid
+     * Initializes Grid with a 2D array to represent nodes
+     */
+    public Grid() {
+        grid = new int[DIM][DIM];
+        // populate the 2D array with empty nodes
+        for (int [] row: grid)
+            Arrays.fill(row, EMPTY_NODE);
+        // default start node will be at (0, 0)
+        startCoordinates = new int[] {0, 0};
+        grid[0][0] = START_NODE;
+        // default end node will be at (DIM - 1, DIM - 1)
+        endCoordinates = new int[] {DIM - 1, DIM - 1};
+        grid[DIM - 1][DIM - 1] = END_NODE;
+    }
+
+    /**
+     * Sets preset grid
+     */
+    public void setPreset(int number) {
+        switch (number) {
+            case 1:
+                grid = new int[][]
+                        {
+                                {1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+                                {1, 1, 2, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+                                {0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
+                                {0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+                                {1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+                                {0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+                                {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0},
+                                {1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+                                {1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+                                {0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
+                                {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+                                {1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1},
+                                {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1},
+                                {1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1},
+                                {1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1},
+                                {1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+                                {0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 1},
+                                {0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+                                {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+                                {1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1}
+                        };
+                break;
+        }
+    }
+
+    /**
+     * Change start node coordinates in the 2D array
+     * @param x     x coordinate of node
+     * @param y     y coordinate of node
+     */
+    public void changeStartNode(int x, int y) {
+        grid[x][y] = START_NODE;
+        startCoordinates = new int[] {x, y};
+    }
+
+    /**
+     * Change end node coordinates in the 2D array
+     * @param x     x coordinate of node
+     * @param y     y coordinate of node
+     */
+    public void changeEndNode(int x, int y) {
+        grid[x][y] = END_NODE;
+        endCoordinates = new int[] {x, y};
+    }
+
+    /**
+     * Add obstacle node in the 2D array
+     * @param x     x coordinate of node
+     * @param y     y coordinate of node
+     */
+    public void addObstacle(int x, int y) {
+        grid[x][y] = OBSTACLE_NODE;
+    }
+
+    public void removeObstacle(int x, int y) {
+        grid[x][y] = EMPTY_NODE;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int row = 0; row < DIM; row++) {
+            for (int column = 0; column < DIM; column++) {
+                ret = ret + Integer.toString(grid[row][column]) + " ";
+                if (column == DIM - 1) ret += "\n";
+            }
+        }
+        return ret;
+    }
 }
--- src/PathfindingVisualizerFX/PathfindingVisualizerPTUI.java
+++ src/PathfindingVisualizerFX/PathfindingVisualizerPTUI.java
@@ -1,9 +1,88 @@
 package PathfindingVisualizerFX;
 
+import java.util.Scanner;
+
 public class PathfindingVisualizerPTUI {
+    // Scanner for input
+    private static Scanner scan;
+    // display message
+    private final static String MESSAGE =
+            "\nPathfinding Visualizer\n----------" +
+            "\nNUMBER CODES\n----------" +
+            "\nEMPTY NODE =\t\t0\nSTART NODE =\t\t1\nEND NODE =\t\t\t2\nOBSTACLE NODE =\t\t3\nVISITED NODE:\t\t4\nCHOSEN PATH:\t\t5";
+    // todo commands for interactions with the grid
+    private final static String COMMANDS = "";
+    // preset choices
+    private final static String PRESETS =
+                    "\nCHOOSE YOUR PRESET (NUMBER):" +
+                    "\n1.\t1 1 0 0 1 0 1 0 0 1 0 0 1 0 1 0 0 0 0 1" +
+                    "\n\t1 1 2 1 0 0 0 0 1 1 0 1 1 0 0 1 1 1 0 1" +
+                    "\n\t0 1 0 0 1 0 0 1 1 0 0 0 0 1 0 0 0 1 0 1" +
+                    "\n\t0 0 0 1 0 1 0 0 1 0 0 1 1 0 1 0 1 0 0 0" +
+                    "\n\t1 1 0 0 0 0 1 0 0 1 0 1 0 0 1 0 1 0 1 1" +
+                    "\n\t0 1 0 1 1 0 0 0 1 0 0 1 0 0 0 0 0 0 1 0" +
+                    "\n\t0 0 0 0 1 1 1 1 1 0 1 0 1 1 1 1 0 1 1 0" +
+                    "\n\t1 1 0 0 1 0 1 0 0 1 0 0 1 0 1 0 0 0 0 1" +
+                    "\n\t1 0 0 1 0 0 0 0 1 1 0 1 1 0 0 1 1 1 0 1" +
+                    "\n\t0 1 0 0 1 0 0 1 1 0 0 0 0 1 0 0 0 1 0 1" +
+                    "\n\t0 0 0 1 0 0 0 0 1 0 0 1 1 0 1 0 1 0 0 0" +
+                    "\n\t1 1 0 0 0 1 0 0 1 1 1 1 1 0 1 0 1 0 1 1" +
+                    "\n\t1 1 0 0 1 1 1 0 1 1 1 1 1 0 1 1 1 0 1 1" +
+                    "\n\t1 1 0 0 1 1 1 0 1 1 1 1 1 0 0 0 0 0 1 1" +
+                    "\n\t1 1 0 0 1 0 1 0 0 0 0 0 0 0 1 0 1 0 0 1" +
+                    "\n\t1 0 0 1 0 0 0 0 1 1 0 1 1 0 0 1 1 1 0 1" +
+                    "\n\t0 1 0 0 1 0 0 1 1 0 0 0 0 0 0 3 0 1 0 1" +
+                    "\n\t0 0 0 1 0 1 0 0 1 0 0 1 1 0 1 0 1 0 0 0" +
+                    "\n\t1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1" +
+                    "\n\t0 0 0 1 1 1 1 1 0 0 1 1 1 0 0 0 0 0 0 1";
+
+    /**
+     * Sleeps for a duration of time so user can catch up
+     */
+    private static void delay() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Reads the user input
+     */
+    public static void input() {
+        String input;
+        Grid grid = new Grid();
+        while (true) {
+            System.out.println("\n" + grid.toString());
+            System.out.print("Enter your command: ");
+            input = scan.nextLine().toUpperCase();
+            switch (input) {
+                case "HELP":
+                    break;
+                case "PRESET":
+                    System.out.println(PRESETS);
+                    grid.setPreset(scan.nextInt());
+                    break;
+                case "EXIT":
+                    System.exit(0);
+                default:
+                    System.out.println("Command not recognized.");
+                    delay();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        scan = new Scanner(System.in);
+        // introductory message
+        System.out.println(MESSAGE);
+        // read commands
+        try {
+            input();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
